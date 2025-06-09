ff7f0089bee1f482949a8e4e8a026852ef7e7740
Raphael <raphael.larsen@proton.me>
2025-06-09 14:31:57 -0400

    Basic 3D minecraft clone voxel generation.

---
+++ generator/structure.gd
@@ -0,0 +1,5 @@
+extends RefCounted
+class_name Structure
+
+var offset: Vector3i = Vector3i.ZERO
+var voxels: VoxelBuffer = VoxelBuffer.new()
---
+++ player/player_head.gd
@@ -0,0 +1,22 @@
+extends Marker3D
+class_name PlayerHead
+
+@export var player: CharacterBody3D
+@export var mouse_sensitivity: float = .004
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	
+	if event is InputEventMouseMotion:
+		
+		var delta_rotation: Vector2 = -event.relative * mouse_sensitivity
+		
+		# yaw
+		player.rotate_y(delta_rotation.x)
+		
+		# pitch
+		var new_rotation_x: float = delta_rotation.y + rotation.x
+		new_rotation_x = clampf(new_rotation_x, - PI / 2, PI / 2)
+		rotation.x = new_rotation_x
